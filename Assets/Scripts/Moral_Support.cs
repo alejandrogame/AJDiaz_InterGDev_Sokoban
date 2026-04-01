90f652e759edfe66ad97c50693fc47a1d089dfe6
alejandrogame <alejandro@truspark.com>
2026-04-01 11:08:28 -0400

    moral_support_draft_texts

--- Assets/Scripts/Moral_Support.cs
+++ Assets/Scripts/Moral_Support.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class Moral_Support : MonoBehaviour
 {
 
-    [SerializeField] public GameObject visitor;
+    [SerializeField] public List<GameObject> visitors = new List<GameObject>(); 
     [SerializeField] public GameObject panel;
     [SerializeField] public TextMeshProUGUI text;
     [SerializeField] public float wiggle_room; // distance offset between this obj and visitor
@@ -13,6 +13,8 @@ public class Moral_Support : MonoBehaviour
     [SerializeField][Range(1, 5)] public float exponential_power;
     [SerializeField] public List<string> texts = new List<string>();
 
+    [SerializeField] public float activation_time = 2.0f; // amount of time to activate
+    private float timer = 0.0f;
     private bool is_displaying_message = false;
 
     void Start()
@@ -22,30 +24,58 @@ public class Moral_Support : MonoBehaviour
     void Update()
     {
         Vector2 this_pos_xz = new Vector2(transform.position.x, transform.position.z);
-        Vector2 visitor_pos_xz = new Vector2(visitor.transform.position.x, visitor.transform.position.z);
 
-        if (Vector2.Distance(this_pos_xz, visitor_pos_xz) < wiggle_room)
+        bool any_visitor_on_tile = false;
+
+        foreach (GameObject current_visitor in visitors)
         {
-            bool space_pressed = Input.GetKeyDown(KeyCode.Space);
-            if (space_pressed && !is_displaying_message && texts.Count > 0)
+            if (current_visitor == null) continue;
+
+            Vector2 visitor_pos_xz = new Vector2(current_visitor.transform.position.x, current_visitor.transform.position.z);
+            float current_distance = Vector2.Distance(this_pos_xz, visitor_pos_xz);
+
+            if (current_distance < wiggle_room)
             {
-                text.text = GetExpRandText();
-                panel.SetActive(true);
-                is_displaying_message = true;
+                any_visitor_on_tile = true;
+                break;
+            }
+        }
+
+        if (any_visitor_on_tile)
+        {
+            if (!is_displaying_message)
+            {
+                timer += Time.deltaTime;
+
+                if (timer >= activation_time && texts.Count > 0)
+                {
+                    text.text = GetExpRandText();
+                    panel.SetActive(true);
+                    is_displaying_message = true;
+                }
+            }
+            else
+            {
+                if (!panel.activeSelf)
+                {
+                    panel.SetActive(true);
+                }
             }
         }
         else
         {
             panel.SetActive(false);
             is_displaying_message = false;
+            timer = 0.0f;
         }
     }
 
     private string GetExpRandText()
     {
-        float exp_random_float = Mathf.Pow(Random.value, exponential_power); // get random #, values weighted more towards 0, less towards 1
-        int index = Mathf.FloorToInt(exp_random_float * texts.Count); // translating to list indeces, all values except 1 will result in index less than 1 here
-        if (index >= texts.Count) // in case index = 1
+        float exp_random_float = Mathf.Pow(Random.value, exponential_power);
+        int index = Mathf.FloorToInt(exp_random_float * texts.Count);
+
+        if (index >= texts.Count)
         {
             index = texts.Count - 1;
         }
